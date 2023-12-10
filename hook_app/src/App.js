cf5d05523badf2e7bd504c5a01900f104ee91cc0
Mayur_G <mayurjacksongold@gmail.com>
2023-12-10 23:54:51 +0530

    update ARRAY of OBJECTS

--- hook_app/src/App.js
+++ hook_app/src/App.js
@@ -1,12 +1,19 @@
-import MyComponent from './MyComponent.jsx'
-import Counter from './Counter.jsx'
+//import MyComponent from './MyComponent.jsx'
+//import MyComponent1 from './MyComponent1.jsx'
+//import MyComponent2 from './MyComponent2.jsx'
+import MyComponent3 from './MyComponent3.jsx'
+//import Counter from './Counter.jsx'
 
 function App() {
 
     return(
     <>
-    <Counter />
-    <MyComponent />
+    {/* <Counter />
+    <MyComponent /> 
+    <MyComponent1/>
+    <MyComponent2/> */}
+
+    <MyComponent3/>
     </>
     );
 }
